dbcde5b62e7edcf505e2673e1aac038a69e00028
Shubhanshu <shubh.aarsay@gmail.com>
2021-01-02 20:39:36 -0500

    Added new constants for intro.

--- src/LocaleConstants/en/GeneralConstants.ts
+++ src/LocaleConstants/en/GeneralConstants.ts
@@ -3,6 +3,15 @@ export class GeneralConstants {
   static ABOUT_ME = 'About me';
   static OWNER_FIRST_NAME = 'Shubhanshu';
   static OWNER_LAST_NAME = 'Aarsay';
-  static OWNER_NAME =
-    GeneralConstants.OWNER_FIRST_NAME + ' ' + GeneralConstants.OWNER_LAST_NAME;
+  static INTRO_PART_1 = `
+  I am a software developer based in Ottawa, Canada. Being a techie and
+  an immigrant has shaped my world view. My interests range from science
+  & technology to politics and ethics. I am a F1 fan, so you can talk to
+  me about motorsports and cars in general.`;
+  static INTRO_PART_2 = `  
+  An avid programmer, I keep exploring new ideas and never stop writing code. I start numerous
+  projects but I never get the chance to complete any -- or that is what
+  I what you to think because I am working on next big thing in private
+  repos.
+  `;
 }
