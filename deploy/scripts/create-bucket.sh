e2bfc40f7bfa1fecb9eab95c6a1b2bbeb3b37090
Shubhanshu <shubh.aarsay@gmail.com>
2021-01-21 19:54:51 -0500

    Now we check for bucket and then upload build to s3.

---
+++ deploy/scripts/create-bucket.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+BUCKET_NAME=shubh-react-apps
+BUCKET_EXISTS=$(aws s3api list-buckets | jq -r '.Buckets[] | select(.Name | contains("shubh-react-apps")) | .Name')
+if  [[ -n "$BUCKET_EXISTS" ]] && [[ "$BUCKET_NAME" == "$BUCKET_EXISTS" ]] ; then
+  echo "The AWS S3 bucket ${BUCKET_NAME} exists"
+else
+  echo "The AWS S3 bucket ${BUCKET_NAME} does not exist, creating bucket"
+  BUCKET_CREATED=$(aws s3api create-bucket --bucket ${BUCKET_NAME} | jq -r ".Location")
+  if [ ! -z ${BUCKET_CREATED} ]; then
+    echo "The AWS S3 bucket ${BUCKET_NAME} created"
+  else
+    echo "Something went wrong in creating bucket"
+    exit 1
+    fi
+fi
