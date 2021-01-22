cfccc07e30d87c135af71eb654bcd21b370ab4f3
Shubhanshu <shubh.aarsay@gmail.com>
2021-01-21 23:50:00 -0500

    Now we deploy using shell script.

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -13,3 +13,7 @@ else
     exit 1
     fi
 fi
+
+echo "Uploading build to S3 bucket ${BUCKET_NAME}"
+aws s3 cp --recursive build ${BUCKET_NAME}/base-web-client/build
+echo "Finished uploading build"
