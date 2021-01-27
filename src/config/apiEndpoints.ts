d37e859a16a1b49f9292e6267d99c612e70d5a4e
Shubhanshu <shubh.aarsay@gmail.com>
2021-01-27 03:23:14 -0500

    Fixing env var loading. I wasn't using REACT_APP prefix.

--- src/config/apiEndpoints.ts
+++ src/config/apiEndpoints.ts
@@ -1,3 +1,5 @@
 export const ENVIRONMENT = process.env.NODE_ENV;
 export const prod = ENVIRONMENT === 'production'; // Anything else is treated as 'dev'
-export const SERVER_URL = process.env.SERVER_URI;
+export const SERVER_URL = prod
+  ? process.env.REACT_APP_SERVER_URI
+  : 'http://localhost:3000';
