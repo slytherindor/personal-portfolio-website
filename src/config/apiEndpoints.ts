0260b46a00dc3ecf6f37e13ce5598b68f576be53
Shubhanshu <shubh.aarsay@gmail.com>
2021-01-27 02:42:31 -0500

    Directly referencing env var.

--- src/config/apiEndpoints.ts
+++ src/config/apiEndpoints.ts
@@ -1,5 +1,3 @@
 export const ENVIRONMENT = process.env.NODE_ENV;
 export const prod = ENVIRONMENT === 'production'; // Anything else is treated as 'dev'
-export const SERVER_URL = prod
-  ? process.env['SERVER_URI']
-  : 'http://localhost:3000';
+export const SERVER_URL = process.env.SERVER_URI;
